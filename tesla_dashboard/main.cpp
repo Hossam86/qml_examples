03be8fe4f5b751c070dd2e575ee7b83a1c2fd49d
Hossam Samir <hossamsamir@outlook.com>
2023-06-04 01:29:02 +0300

    setting system context to controller

--- tesla_dashboard/main.cpp
+++ tesla_dashboard/main.cpp
@@ -1,10 +1,14 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
 
+#include<Controllers/system.h>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    System m_systemHandler;
 
     QQmlApplicationEngine engine;
     const QUrl url(u"qrc:/tesla_dashboard/Main.qml"_qs);
@@ -12,6 +16,9 @@ int main(int argc, char *argv[])
         &app, []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
     engine.load(url);
+
+    QQmlContext *context(engine.rootContext());
+    context->setContextProperty("systemHandler", &m_systemHandler);
 
     return app.exec();
 }
